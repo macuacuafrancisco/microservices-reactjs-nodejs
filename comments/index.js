7264af50ad96b63e7ea6e7206cfe4d03fae2b595
Francisco Macuacua <macuacuafrancisco@gmail.com>
2022-08-07 23:49:09 +0200

    first commit

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {randomBytes} = require('crypto')
+
+const app = express()
+app.use(bodyParser.json())
+
+const commentsbyPostId = {}
+
+app.get('/posts/:id/comments', (req,res)=>{
+    res.send(commentsbyPostId[req.params.id] || [])
+});
+
+
+app.post('/posts/:id/comments', (req,res)=>{
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body
+
+    const comments = commentsbyPostId[req.params.id] || []
+
+    comments.push({id: commentId , content})
+
+    commentsbyPostId[req.params.id] = comments
+
+    res.status(201).send(comments)
+});
+
+
+app.listen(4001, ()=>{
+    console.log('Listenning on 40001')
+})
