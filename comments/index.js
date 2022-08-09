b96cf4d422ca0a2ddeb58e1ebb19bf4625cea290
Francisco Macuacua <macuacuafrancisco@gmail.com>
2022-08-09 05:21:37 +0200

    Eventos

--- comments/index.js
+++ comments/index.js
@@ -1,31 +1,51 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const {randomBytes} = require('crypto')
-var cors = require('cors')
+const cors = require('cors')
+const axios = require('axios')
+
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-const commentsbyPostId = {}
+const commentsByPostId = {}
 
 app.get('/posts/:id/comments', (req,res)=>{
-    res.send(commentsbyPostId[req.params.id] || [])
+    res.send(commentsByPostId[req.params.id] || [])
 });
 
 
-app.post('/posts/:id/comments', (req,res)=>{
+app.post('/posts/:id/comments', async (req,res)=>{
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body
 
-    const comments = commentsbyPostId[req.params.id] || []
+    const comments = commentsByPostId[req.params.id] || []
 
     comments.push({id: commentId , content})
 
-    commentsbyPostId[req.params.id] = comments
+    commentsByPostId[req.params.id] = comments
 
+    /*
+    await axios.post('http://localhost:4005/events',{
+        type:'CommentCreated',
+        data:{
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    }).catch(err=>{
+        console.log(err)
+    })*/
+    
     res.status(201).send(comments)
 });
+
+app.post('/events', (req,res)=>{
+    console.log('Received Event', req.body.type)
+
+    res.send({})
+})
 
 
 app.listen(4001, ()=>{
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const {randomBytes} = require('crypto')
 var cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,7 +15,7 @@ app.get('/posts', (req,res)=>{
 });
 
 
-app.post('/posts', (req,res)=>{
+app.post('/posts', async (req,res)=>{
     const id = randomBytes(4).toString('hex');
     const {title} = req.body
 
@@ -22,9 +23,23 @@ app.post('/posts', (req,res)=>{
         id, title
     }
 
+    await axios.post('http://localhost:4005/events',{
+        type:'PostCreated',
+        data:{
+            id, title
+        }
+    }).catch(err=>{
+        console.log(err)
+    })
+
     res.status(201).send(posts[id])
 });
 
+app.post('/events', (req,res)=>{
+    console.log('Received Event', req.body.type)
+
+    res.send({})
+})
 
 app.listen(4000, ()=>{
     console.log('Listenning on 40000')
